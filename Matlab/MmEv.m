559a0959b2fa8671b2cdadc95f53d85c346db8f7
jakobfinnvold <55697129+jakobfinnvold@users.noreply.github.com>
2022-10-25 21:13:23 +0200

    Update MmEv.m

--- Matlab/MmEv.m
+++ Matlab/MmEv.m
@@ -1,4 +1,13 @@
 clear; close all; clc; 
+%Parameters
+rd = 0.225;
+m = 9000;
+g = 9.81;
+dd = 0.45;
+nsh = 3;
+i_g = 46;
+mu_eq = 0.15;
+w0 = 5; 
 
 %Time integration
 Time = 0.0;
@@ -9,14 +18,19 @@ StepTime = 1e-4;
 while Time < EndTime
     %Platform position
     Zp = 0.8*sin(pi/5 * Time);
-    ZpDot = (4*pi*cos(pi*Time/5)/25); 
-    ZpDotDot = -(4*pi*sin(pi*Time/5)/125); 
+    ZpDot = ((4*pi*cos(pi*Time/5))/25); 
+    ZpDotDot = -((4*pi*sin(pi*Time/5))/125); 
 
     %Mass position
     Zm = -0.8*sin((pi)/5 * Time);
     ZmDot = -(4*pi*cos(pi*Time/5)/25);
     ZmDotDot = (4*pi*sin(pi*Time/5)/125); 
 
+    % Wm
+    wm = ZmDot/rd; 
+
+    %Mm
+    Mm = ((m*g*dd)/(2*nsh*2*i_g)) * (1 + mu_eq*tanh(wm/w0)); 
 
     TimePlot(Counter) = Time;
     zPlot(Counter) = Zp;
@@ -25,6 +39,8 @@ while Time < EndTime
     zmPlot(Counter) = Zm; 
     zmDotPlot(Counter) = ZmDot; 
     zmDotDotPlot(Counter) = ZmDotDot; 
+    wmPlot(Counter) = wm;
+    MmPlot(Counter) = Mm;
 
     Time = Time + StepTime;
     Counter = Counter + 1; 
@@ -51,9 +67,11 @@ plot(TimePlot, wPlot);
 xlabel('Time in seconds');
 ylabel('Velocity in m/s'); 
 hold on
+
 plot(TimePlot, zmDotPlot); 
 t1vel = max(zmDotPlot); 
 hold on
+
 plot(4.9932, t1vel, 'o', 'MarkerSize', 10);
 legend('Velocity of platform', 'Velocity of mass', 'Max Velocity at t = 4.9932')
 
@@ -62,6 +80,19 @@ plot(TimePlot, aPlot);
 xlabel('Time in seconds');
 ylabel('Acceleration in m/s^2'); 
 
+figure
+plot(TimePlot, wmPlot); 
+xlabel('Time in seconds');
+ylabel('Velocity in rad/s'); 
+
+figure
+plot(TimePlot, MmPlot); 
+xlabel('Time in seconds');
+ylabel('Motor Moment in Nm'); 
+
 
 a = max(abs(aPlot)); 
 w = max(abs(wPlot)); 
+wmMax = max(abs(wmPlot)); 
+
+
