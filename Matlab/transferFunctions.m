7ebde757ea2265212288b2f91c94516ce6d55e5f
PerHenrikHardeberg <perhenrikh@hotmail.com>
2022-11-11 12:18:24 +0100

    lagt til pid HPU

--- Matlab/transferFunctions.m
+++ Matlab/transferFunctions.m
@@ -3,7 +3,17 @@ clc; clear; close all;
 %HPU
 omega_hpu = 6; %[rad/s], natural frequency
 zeta_hpu = 1; %[-] damping 
+K = 1;%constant SET AS 1. 
 
 s = tf('s');
 
+hpuTF = K*omega_hpu^2/(s^2+2*s*zeta_hpu*omega_hpu+omega_hpu^2);
 
+step(hpuTF) 
+grid 
+
+figure
+
+bode(hpuTF)
+
+grid
